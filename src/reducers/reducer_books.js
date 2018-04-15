7864843bfc0703907bc0e0c43c3a5156fa878c38
Yasin Shuman <email@yasin.io>
2018-04-14 19:40:25 -0700

    saving progress unable to make detail show up--waiting for help

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "js: the good parts", pages: 101 },
+    { title: "harry potter", pages: 29 },
+    { title: "the dark tower", pages: 31 },
+    { title: "eloquent ruby", pages: 200 }
+  ];
+}
